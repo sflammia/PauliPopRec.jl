be305aa75bca37dbd1843d759a457f77684d52dd
Steve Flammia <sflammia@gmail.com>
2022-01-10 16:08:27 -0800

    bug fix and simplify

--- PauliEstViaPopRec.jl
+++ PauliEstViaPopRec.jl
@@ -36,9 +36,11 @@ end
 # Here w[k] = (-1/2)^(k-1) / m is the weighing factor for the estimator. 
 # This estimator assumes B is not the all-0 vector already. 
 function EstProb(B::Vector{Int8},A::Matrix{Int8},R::Matrix{Int8},w::Vector{Float64},t::Integer)::Float64
+    if all(B .== 0)
+        return w' * (counts(sum(R, dims = 2),0:t))
+    end
     AB = star(A, B' .+ zeros(Int8,size(A)) )
-    ABR = AB .⊻  R
-    w' * (counts(sum(ABR, dims = 2),0:t) - counts(sum(AB, dims = 2),0:t) )
+    w' * (counts(sum(AB .⊻  R, dims = 2),0:t) - counts(sum(AB, dims = 2),0:t) )
 end
 
 # Estimate the whole channel using branch and prune from probe states A
@@ -48,9 +50,9 @@ end
 function EstChan(A::Matrix{Int8},R::Matrix{Int8},𝛿::Float64)::Tuple{Vector{Float64}, Matrix{Int8}}
     (m,n) = size(A)
     p = Float64[]
-    P = Vector{Int8}[[0],[1],[2],[3]]
+    P = Vector{Int8}[[]]
     w = (-1/2).^(0:n) / m
-    for j = 2:n
+    for j = 1:n
         q = Float64[]
         Q = Array{Int8,1}[]
         AA = A[:,1:j]
@@ -70,13 +72,19 @@ function EstChan(A::Matrix{Int8},R::Matrix{Int8},𝛿::Float64)::Tuple{Vector{Fl
         p = q
         P = Q
     end
-    # finally, check the zero string
-    est = w' * counts(sum(R, dims = 2),0:n)
-    if est > 𝛿
-        pushfirst!(p, est)
-        pushfirst!(P, zeros(Int8,n))
-    end
     (p,vcat(P'...))
+end
+
+# Estimate 1 std error bars using the jackknife estimator for variance
+function EstChanErrorBars(A::Matrix{Int8},Q::Matrix{Int8},R::Matrix{Int8})::Vector{Float64}
+    k = size(Q)[1]
+    (m,n) = size(A)
+    s = zeros(k)
+    for j = 1:k
+        AQ = star(A, Q[j,:]' .+ zeros(Int8, (m,n)) )
+        s[j] = std(dropdims((-1/2).^sum(AQ .⊻  R,dims=2) - (-1/2).^sum(AQ,dims=2),dims=2))
+    end
+    s/sqrt(m)
 end
 
 # represent a sparse Pauli channel as a dictionary
@@ -84,16 +92,18 @@ sparseP(p::Vector{Float64},P::Matrix{Int8})::Dict{Vector{Int8},Float64} =
     Dict{Vector{Int8},Float64}( [P[j,:] for j in 1:size(P)[1] ] .=> p )
 
 # compute the total variation distance between two Pauli dictionaries
-tvd(p::Vector{Float64},P::Matrix{Int8},q::Vector{Float64},Q::Matrix{Int8})::Float64 = 
-    tvd(sparseP(p,P),sparseP(q,Q))
 tvd(pP::Dict{Vector{Int8},Float64},qQ::Dict{Vector{Int8},Float64})::Float64 = 
     sum(abs,values(merge(-,pP,qQ)))/2
 
-# Brute force compute the estimator on *all* the Paulis. Don't use with large n!
-EstAll(A::Matrix{Int8},R::Matrix{Int8})::Vector{Float64} = EstAll(A,R,size(A)[1],size(A)[2])
-function EstAll(A::Matrix{Int8},R::Matrix{Int8},m::Integer,n::Integer)::Vector{Float64}
+# Brute force compute the estimator on *all* the Paulis. Not the same as EstChan(A,R,0.0).
+# It only keeps the final results with positive estimates. Don't use with large n!
+function EstAll(A::Matrix{Int8},R::Matrix{Int8})::Tuple{Vector{Float64}, Matrix{Int8}}
+    (m,n) = size(A)
     @assert n < 6
     w = (-1/2).^(0:n) / m
     p0 = w' * counts(sum(R, dims = 2),0:n)
-    pushfirst!([EstProb(reverse(digits(Int8, j,base=4,pad=n)),A,R,w,n) for j = 1:4^n-1], p0)
+    s = pushfirst!([EstProb(reverse(digits(Int8, j,base=4,pad=n)),A,R,w,n) for j = 1:4^n-1], p0)
+    S = vcat(reverse.(digits.(Int8, findall(x -> x > 0, s) .- 1, base=4,pad=n))'...)
+    s = s[s .> 0]
+    (s,S)
 end
